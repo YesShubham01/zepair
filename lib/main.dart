d0ee3ed403bb9d9daad1c22fb1f4832d54463ddb
Pranjalbharat <pranjalbharat07@gmail.com>
2025-02-15 14:56:50 +0530

    Service Details

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:zepair/modules/Contact%20Page/contactUs.dart';
 import 'package:zepair/modules/Feedback%20Page/feedback_page.dart';
 import 'package:zepair/modules/Home%20Pages/home_page.dart';
 import 'package:zepair/modules/Home%20Pages/home_screen.dart';
+import 'package:zepair/modules/Services/service_detail.dart';
 import 'package:zepair/modules/Splash%20Page/splash_page.dart';
 import 'package:zepair/utils/constants/colors.dart';
 
@@ -26,7 +27,10 @@ class ZepairApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
 
       // call here the page you are working on.
-      home: const FeedbackPage(),
+      home: const ServiceDetailPage(
+          service: 'AC Service',
+          description:
+              'Get your AC cleaned and serviced to extend its lifespan and reduce power consumption.'),
     );
   }
 }

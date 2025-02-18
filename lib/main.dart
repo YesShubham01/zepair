c314caf75ba44165a2f8c32540ef76f793521af8
Shubham Puhal <shubhampuhal01@gmail.com>
2025-02-18 23:32:59 +0530

    FireBase integrated with Project. 📡

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:zepair/modules/Home%20Pages/home_page.dart';
-import 'package:zepair/modules/Home%20Pages/home_screen.dart';
-import 'package:zepair/modules/Manage%20Addresses%20Page/manage_addresses_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:zepair/firebase_options.dart';
 import 'package:zepair/modules/Splash%20Page/splash_page.dart';
 import 'package:zepair/utils/constants/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ZepairApp());
 }
 
